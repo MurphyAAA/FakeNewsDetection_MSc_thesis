2ecb1fa2198100d8619694a108de76bd52269a7f
MYF <1034934778@qq.com>
2024-05-09 13:55:45 +0200

    check image

--- preprocessing/check_image.py
+++ preprocessing/check_image.py
@@ -18,20 +18,24 @@ def read_file(data_path, filename):
     return df
 
 
-def check_file(filename, dataframe):  # 检查 train,val,test数据集中的图片是否能正常打开，不能打开则将图片名写入文件filename
-    if not os.path.isfile(f'data/Fakeddit/{filename}'):
-        with open(filename, "w") as file:
-            print(f"starting check 【{filename}】")
-            print(os.getcwd())
-            for index in dataframe["id"]:
-                img_path = f'data/Fakeddit/public_image_set/{index}.jpg'
-                try:
-                    Image.open(img_path)
-                except Exception as e:
-                    file.write(f'{index} - {e}')
-                    continue
-    else:
-        print(f"file {filename} exists")
+def check_file(filename, dataframe, res):  # 检查 train,val,test数据集中的图片是否能正常打开，不能打开则将图片名写入文件filename
+    # if not os.path.isfile(f'data/Fakeddit/{filename}'):
+
+    print(f"starting check 【{filename}】")
+    print(os.getcwd())
+    for index in dataframe["id"]:
+        img_path = f'data/Fakeddit/public_image_set/{index}.jpg'
+        try:
+            img = Image.open(img_path)
+        except Exception as e:
+            # file.write(f'{index} - {e}')
+            res[index] = e
+            continue
+    # return res
+
+
+# else:
+#     print(f"file {filename} exists")
 
 
 if __name__ == '__main__':
@@ -43,7 +47,13 @@ if __name__ == '__main__':
     df_val = df_val[["clean_title", "id", "2_way_label"]]
     df_test = df_test[["clean_title", "id", "2_way_label"]]
 
-    check_file("filter_image_train.txt", df_train)
-    check_file("filter_image_val.txt", df_val)
-    check_file("filter_image_test.txt", df_test)
+    res1={}
+    res2={}
+    res3={}
+    check_file("filter_image_train.txt", df_train, res1)
+    check_file("filter_image_val.txt", df_val, res2)
+    check_file("filter_image_test.txt", df_test, res3)
+    print(res1)
+    print(res2)
+    print(res3)
     print("---end---")
