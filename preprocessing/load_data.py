1020873fa7a4df7622ea1caf7f8e8939531f9079
MYF <1034934778@qq.com>
2024-06-29 11:45:04 +0200

    clip

--- preprocessing/load_data.py
+++ preprocessing/load_data.py
@@ -99,7 +99,13 @@ class CustomDataset_Clip(Dataset):
 
         # try:
         img = Image.open(img_path).convert("RGB")
-        print(f"image:\n{img}") #看看有问题的image有啥不同！！！！
+        print(f"index:{index}, image id:{self.img_id[index]}, image:\n{img}") #看看有问题的image有啥不同！！！！
+        convert_tensor = transforms.ToTensor()
+        tensor = convert_tensor(img)
+# 打印张量信息
+        print(f"张量形状: {tensor.shape}")  # 输出： torch.Size([3, 高度, 宽度])
+        print(f"张量数据类型: {tensor.dtype}")  # 输出： torch.float32
+        pdb.set_trace()
         # 尝试一下循环open所有的图片，看看所有的图片是不是都能打开，应该可以。。。vit都行
         # except Image.DecompressionBombWarning:
         #     print(f"图片过大 {self.img_id[index]}")
