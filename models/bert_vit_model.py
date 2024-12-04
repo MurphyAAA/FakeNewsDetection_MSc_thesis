a9e5dc7f95eb1fc93635b98b258e1a493282d927
MYF <1034934778@qq.com>
2024-12-04 16:07:25 +0100

    fine-tune vis-SA model

--- models/bert_vit_model.py
+++ models/bert_vit_model.py
@@ -11,6 +11,32 @@ import torch
 from transformers import BertModel, ViTModel, BertTokenizer, ViTImageProcessor, AutoModel, AutoTokenizer, \
     AutoModelForSequenceClassification, AutoImageProcessor, AutoModelForImageClassification
 
+class VisualSentimentModel(torch.nn.Module):
+    def __init__(self):
+        super(VisualSentimentModel, self).__init__()
+        self.visual_sentiment = AutoModelForImageClassification.from_pretrained("kittendev/visual_emotional_analysis")
+        self.category_encoder = torch.nn.Sequential(
+            torch.nn.Linear(768, 768),
+            torch.nn.BatchNorm1d(768),
+            torch.nn.ReLU(),
+
+            torch.nn.Linear(768, 768),
+            torch.nn.BatchNorm1d(768),
+            torch.nn.ReLU(),
+
+            torch.nn.Linear(768, 768),
+            torch.nn.BatchNorm1d(768),
+            torch.nn.ReLU()
+        )
+        self.classifier = torch.nn.Linear(768, 3) # fc
+
+    def forward(self, pixel_values):
+        output = self.visual_sentiment(pixel_values=pixel_values, output_hidden_states=True)
+        last_hidden_states = output['hidden_states'][-1]
+        vis_emo_embeds = last_hidden_states[:, 0, :]
+        vis_emo_embeds = self.category_encoder(vis_emo_embeds)
+        x = self.classifier(vis_emo_embeds)
+        return x,vis_emo_embeds
 
 class Bert_VitClass(torch.nn.Module):
     def __init__(self, opt):
@@ -18,13 +44,18 @@ class Bert_VitClass(torch.nn.Module):
         self.text_encoder = BertModel.from_pretrained('bert-base-uncased')  # embedding
         self.img_encoder = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k',num_labels=int(opt['label_type'][0]))
         self.text_sentiment= AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-        self.visual_sentiment= AutoModelForImageClassification.from_pretrained("kittendev/visual_emotional_analysis")
+        # self.visual_sentiment= AutoModelForImageClassification.from_pretrained("kittendev/visual_emotional_analysis")
+        self.visual_sentiment = VisualSentimentModel()
         self.processors = {
             'bert': BertTokenizer.from_pretrained('bert-base-uncased'),
             'vit': ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k'),
             'text_sentiment': AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest"),
             'visual_sentiment': AutoImageProcessor.from_pretrained("kittendev/visual_emotional_analysis")
         }
+        fileName = f'{opt["output_path"]}/checkpoint_SA_model.pth'
+        checkpoint = torch.load(fileName)
+        self.visual_sentiment.load_state_dict(checkpoint['model'])
+        # print(self.visual_sentiment)
 
         # self.dropout = torch.nn.Dropout(0.3)
         self.emotion_layer = torch.nn.Linear(768, 384)
@@ -41,15 +72,15 @@ class Bert_VitClass(torch.nn.Module):
         _, img_embeds = self.img_encoder(pixel_values=pixel_values, return_dict=False) # 16*768
         # _, emo_embeds = self.sentiment_model1(input_ids=emo_ids, attention_mask=emo_mask, return_dict=False)
         txt_emo_output = self.text_sentiment(input_ids=emo_ids, attention_mask=emo_mask, output_hidden_states=True) # 13* 16*100*768
-        vis_emo_output = self.visual_sentiment(pixel_values=pixel_values_emo, output_hidden_states=True) # 之后试一下用pixel_values
+        _,vis_emo_embeds = self.visual_sentiment(pixel_values=pixel_values_emo) # 之后试一下用pixel_values
         # vis_emo_output = self.visual_sentiment(pixel_values=pixel_values, output_hidden_states=True) # 之后试一下用pixel_values
         hidden_states = txt_emo_output['hidden_states']  # 16*100*768
         last_hidden_states = hidden_states[-1]
         # emo_embeds = last_hidden_states.mean(dim=1) # pooling 16*768 .max(dim=1).values
         txt_emo_embeds = last_hidden_states[:, 0, :] # cls token
-        hidden_states = vis_emo_output['hidden_states']  # 16*100*768
-        last_hidden_states = hidden_states[-1]
-        vis_emo_embeds = last_hidden_states[:, 0, :]
+        # hidden_states = vis_emo_output['hidden_states']  # 16*100*768
+        # last_hidden_states = hidden_states[-1]
+        # vis_emo_embeds = last_hidden_states[:, 0, :]
 
         combined_output = torch.cat((text_embeds, img_embeds), dim=1) # 试一下不直接将emotion的embedding拼起来，直接让模型返回text_embed和emo_embed，计算L2loss，让原本的模型得到的text_embed能更好的提取情绪信息 (embedding distillation)
         output = self.category_classifier(combined_output)
