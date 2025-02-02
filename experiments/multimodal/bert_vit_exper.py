f145cac1233352c8fe58722e4e407916971754de
MurphyAAA <s300570@studenti.polito.it>
2025-02-02 17:09:16 +0100

    remove fa

--- experiments/multimodal/bert_vit_exper.py
+++ experiments/multimodal/bert_vit_exper.py
@@ -100,7 +100,8 @@ class Bert_VitExperiment:
             vis_emo_loss = torch.mean(1 - self.cosine_similarity(img_embeds, vis_emo_embeds))
             txt_intent_loss = torch.mean(1 - self.cosine_similarity(text_embeds, txt_intent_embeds))
             feature_alignment_loss = torch.mean(1 - self.cosine_similarity(text_embeds, img_embeds))
-            L = self.w[0] * loss + self.w[1] * feature_alignment_loss + self.w[2] * txt_emo_loss + self.w[3] * vis_emo_loss + self.w[4] * txt_intent_loss
+            # L = self.w[0] * loss + self.w[1] * feature_alignment_loss + self.w[2] * txt_emo_loss + self.w[3] * vis_emo_loss + self.w[4] * txt_intent_loss
+            L = self.w[0] * loss + self.w[2] * txt_emo_loss + self.w[3] * vis_emo_loss + self.w[4] * txt_intent_loss
             # 计算一个text-embedding和img-embedding的余弦相似度加到loss里，
             # L = self.w[0] * loss + self.w[1] * txt_emo_loss
             self.writer.add_scalar(f"loss_{self.opt['label_type']}", L.item(), epoch*len(self.train_loader) + idx)
